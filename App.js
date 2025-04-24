c4c0d21237787c8c74b79c1f07154736b865ca50
pgomezzy <palp53412@gmail.com>
2025-04-24 15:32:09 -0300

    Primeiro Commit

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, View, Text, StyleSheet, Image, TouchableOpacity } from "react-native"
+import {  useNavigation, NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Button } from "@react-navigation/elements";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const Stack = createNativeStackNavigator();
+
+function PaginaInicial() {
+  const navigation = useNavigation();
+
+  return(
+    
+    <SafeAreaView style={styles.conteiner}>
+      <Image style={styles.logo} source={require('./src/assets/guardianLogo.png')}/>
+      <View>
+        <Text style={styles.textoCabeçalho}>Fique Seguro!!</Text>
+      </View>
+      <View>
+        <Text style={styles.textos}>Use nosso app para verificar se seu arquivo ou url são confiáveis!</Text>
+      </View>
+      <View style={styles.botoes}>
+        <Button onPress={() => navigation.navigate('Arquivo')}>Arquivo</Button>
+        <Button onPress={() => navigation.navigate('URL')}>URL</Button>
+      </View>
+    </SafeAreaView>
+  )
+
+}
+export default function App(){
+
+return(
+  <NavigationContainer >
+    <Stack.Navigator initialRouteName='Home'>
+      <Stack.Screen name='Home' component={PaginaInicial}/>
+      <Stack.Screen name='Arquivo' component={ArquivoPage}/>
+      <Stack.Screen name='URL' component={UrlPage}/>
+    </Stack.Navigator>
+  </NavigationContainer>
+)
+
 }
 
+function UrlPage(){
+  const navigation = useNavigation();
+
+  return(
+    <View>
+      <Text>uh ere eleeeeeeeeee</Text>
+    </View>
+  )
+}
+function ArquivoPage() {
+  const navigation = useNavigation();
+
+  return(
+
+    <View>
+      <Text> arquivooo </Text>
+    </View>
+  )
+}
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  conteiner: {
+    flex:1,
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#031a32",
   },
-});
+  textoCabeçalho: {
+    color: '#a6a6a6', 
+    fontSize: 20
+  },
+  logo: {
+    width: 300, 
+    height: 170,
+  },
+  textos: {
+    justifyContent: 'center',
+    textAlign: 'center',
+    fontSize : 15,
+    color: '#a6a6a6'
+  },
+  botoes: {
+    flexDirection: 'row',
+    gap: 30,
+    marginBottom: 10,
+    marginTop: 10,
+  }
+})
---
+++ src/views/Arquivo.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function App(){
+
+    
+    return(
+        <View>
+            <Text>ksksksk</Text>
+        </View>
+    )
+}
